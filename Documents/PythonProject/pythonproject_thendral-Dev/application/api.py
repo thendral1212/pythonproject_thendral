be061712bfbd2058f7fdaee1b19ac1cd74a2283c
Thendral <thendral.park@gmail.com>
2021-09-04 12:02:58 +0530

    api

---
+++ Documents/PythonProject/pythonproject_thendral-Dev/application/api.py
@@ -0,0 +1,61 @@
+from flask.globals import request
+from application.app import app
+
+# Import your models here
+from application.models import db, Question
+
+@app.route("/")
+def home():
+    return {"Status": "Success"}, 200 
+
+# Write your API endpoints here
+@app.route("/question", methods=["POST"])
+def question():
+
+    """Craete Question
+    {
+        "title" = "This is title",
+        "Description" = "Descrition123"
+    } """
+
+    params = request.json
+    title = params.get("title")
+    Description = params.get("Description")
+ 
+    question = Question(title=title,Description=Description)
+
+    # Make an actual entry in DB
+    db.session.add(question)
+    db.session.commit()
+    return {
+        "Status": "Success",
+        "data": {
+            #"id": question.id,
+            "title": question.title,
+            "Description": question.Description,
+        },
+    }
+
+@app.route("/questions", methods=["GET"])
+def get_all_questions():
+    questions = Question.query.filter().all()
+    data = []
+    for q in questions:
+        data.append({
+            #"id": question.id,
+            "title": q.title,
+            "Description": q.Description,
+        })
+        return {"Status": "Success","data":data},200
+
+@app.route("/question/<int:id>", methods=["GET"])
+def get_question_by_id(id):
+    questions = Question.query.filter_by(id=id).first()
+    data = []
+    for q in questions:
+        data.append({
+            "id": q.id,
+            "title": q.title,
+            "Description": q.Description,
+        })
+        return {"Status": "Success","data":data},200
